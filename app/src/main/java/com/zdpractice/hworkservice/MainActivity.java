bb16293f617eb237144aa9e95c54624b3b262435
BrainDarren <930387850@qq.com>
2016-08-15 14:41:05 +0800

    添加testA字符串

--- app/src/main/java/com/zdpractice/hworkservice/MainActivity.java
+++ app/src/main/java/com/zdpractice/hworkservice/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //dev
+        //添加testA
     }
 }
